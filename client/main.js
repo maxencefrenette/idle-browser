19420a6fa4c8ac60905e0572e27f3120d62f59da
Maxence Frenette <maxence.frenette@gmail.com>
2021-06-10 19:29:16 -0700

    Add basic side-by-side view of 2 games

--- client/main.js
+++ client/main.js
@@ -1,16 +1,31 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, BrowserView } = require('electron');
 const path = require('path');
 
 function createWindow() {
+    const windowWidth = 1200;
+    const windowHeight = 850;
+
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 850,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
     });
 
     win.loadFile('index.html');
+
+    const view = new BrowserView();
+    win.addBrowserView(view);
+    view.setBounds({ x: 0, y: 0, width: windowWidth/2, height: windowHeight });
+    view.setAutoResize({ width: true, height: true, horizontal: true, vertical: true });
+    view.webContents.loadURL('http://orteil.dashnet.org/cookieclicker/');
+
+    const view2 = new BrowserView();
+    win.addBrowserView(view2);
+    view2.setBounds({ x: windowWidth/2, y: 0, width: windowWidth/2, height: windowHeight });
+    view2.setAutoResize({ width: true, height: true, horizontal: true, vertical: true });
+    view2.webContents.loadURL('https://ducdat0507.github.io/prestreestuck/');
 }
 
 app.whenReady().then(() => {
